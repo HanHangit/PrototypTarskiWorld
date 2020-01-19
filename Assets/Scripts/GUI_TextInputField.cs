68e0cb9956358360ed5411779479291e935ba4f6
12Cy <christian.sandkaemper@gmail.com>
2020-01-19 16:35:01 +0100

    Adding first GUI elements Importing Textmesh

---
+++ Assets/Scripts/GUI_TextInputField.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUI_TextInputField : MonoBehaviour
+{
+    [SerializeField]
+    List<TMPro.TMP_InputField> _inputFields = default;
+
+    private void OnValidate()
+    {
+        _inputFields = new List<TMPro.TMP_InputField>(GetComponentsInChildren<TMPro.TMP_InputField>());
+    }
+
+    private void Awake()
+    {
+        foreach (var item in _inputFields)
+        {
+            item.onEndEdit.AddListener(EndEdit);
+        }
+    }
+
+    private void EndEdit(string arg0)
+    { 
+       Debug.Log("EndEditing");
+    }
+}
