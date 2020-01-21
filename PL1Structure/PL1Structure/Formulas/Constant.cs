2ab8b628964a181cb07a242c9b96073f27b9807d
HanHan <hauffej@yahoo.de>
2020-01-21 23:11:11 +0100

    Added modelParser and gitignores

---
+++ PL1Structure/PL1Structure/Formulas/Constant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PL1Structure
+{
+    public class Constant : Formula, IArgument
+    {
+        public Constant(string argument) : base(argument)
+        {
+        }
+
+        public string Value => _formula;
+    }
+}
