fbeeb93a2a793fc854bf4b65753e1d1380a6335f
Johannes Hauffe <johannes.hauffe@nexus-marabu.de>
2020-02-02 11:31:25 +0100

    Updated Unity version Added constant fields

---
+++ PL1Structure/PL1Structure/DataStructures/DataFields.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PL1Structure.DataStructures
+{
+    public class DataFields
+    {
+        public const string TET = "Tet";
+        public const string CUBE = "Cube";
+
+        public const string CONSTANT_A = "a";
+        public const string CONSTANT_B = "b";
+    }
+}
